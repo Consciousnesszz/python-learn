fdbac6deb3b9aa09d2629591af7367f2182d0765
Consciousnesszz <consciousnesszz1@gmail.com>
2018-08-08 15:31:20 +0800

    module

--- firstModule.py
+++ firstModule.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
 'my first module'
@@ -7,10 +7,20 @@ __author__ = 'John Zhang'
 
 import sys
 
+# 兼容引入外部模块
+try:
+    import json # python >= 2.6
+except ImportError:
+    import simplejson as json # python <= 2.5
+
+# 私有函数命名
+def _privateFunc():
+  print('1 args')
+
 def test():
   args = sys.argv
   if len(args) == 1:
-    print('1 args')
+    _privateFunc()
   elif len(args) == 2:
     print('second args %s' % args[1])
   else:
